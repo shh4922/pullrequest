8b758ef900a98bdd03ead04fee4e0275a34b2f76
hyeonho <gusgh4922@gmail.com>
2023-06-02 10:22:06 +0900

    Initial Commit

---
+++ pullrequest/pullrequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  pullrequestApp.swift
+//  pullrequest
+//
+//  Created by Hyeonho on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct pullrequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
