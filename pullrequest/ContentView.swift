c34a978309cd8913dfb5951039336826af0a332a
hyeonho <gusgh4922@gmail.com>
2023-06-02 10:38:07 +0900

    test1

--- pullrequest/ContentView.swift
+++ pullrequest/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("Hello, world!")
             Text("Hello, world!")
+            Text("Hello, world!")
         }
         .padding()
     }
